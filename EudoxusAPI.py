f43aa6a55d4702506c28d09c784b168be65307da
R3try0 <81508183+R3try0@users.noreply.github.com>
2022-03-17 12:33:22 -0900

    Update EudoxusAPI.py

--- EudoxusAPI.py
+++ EudoxusAPI.py
@@ -1,7 +1,9 @@
-from pyvirtualdisplay import Display
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait as wait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from time import sleep
 import tkinter
 import os
 #from Crypto.Cipher import AES
@@ -15,154 +17,172 @@ import sys
 #from pyvirtualdisplay import Display
 #display = Display(visible=0, size=(1680,1050))
 #display.start()
-
+def to_dict(a):
+    it = iter(a)
+    res_dct = dict(zip(it, it))
+    return res_dct
+    
 def Main():
-        def to_dict(a):
-            it = iter(a)
-            res_dct = dict(zip(it, it))
-            return res_dct
 
-        driver = webdriver.Firefox()
+    driver = webdriver.Firefox()
 
-        url = 'https://eudoxus.gr/'
-        driver.get(url)
+    url = 'https://eudoxus.gr/'
+    driver.get(url)
 
-        #sleep(.1)
-        wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-        driver.find_element_by_link_text('Έλεγχος Εισόδου Φοιτητή').click()
-        while True:
-            try:
-                driver.find_element_by_link_text('Έλεγχος Εισόδου Φοιτητή').click()
-            except:
-                pass
-                #print('except 1')
-            finally:
-                #print('finally 1')
-                break
+    #sleep(.1)
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+    while True:
+        try:
+            driver.find_element_by_link_text('Έλεγχος Εισόδου Φοιτητή').click()
+        except:
+            pass
+            #print('except 1')
+        finally:
+            #print('finally 1')
+            break
 
-        #sleep(.1)
-        wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-        driver.find_element_by_link_text('εδώ').click()
+    #sleep(.1)
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
 
-        #sleep(.1)
-        wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+    while True:
+        try:
+            driver.find_element_by_link_text('εδώ').click()
+        except:
+            pass
+            #print('except 2')
+        finally:
+            #print('finally 2')
+            break
+    while True:
         try:
             driver.find_element_by_link_text('Ελληνικά').click()
         except:
             pass
-        while True:
-            try:
-                driver.find_element_by_link_text('εδώ').click()
-            except:
-                pass
-                #print('except 2')
-            finally:
-                #print('finally 2')
+            #print('except 3')
+        finally:
+            #print('finally 3')
+            break
+
+    #sleep(.1)
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+    # select = wait(driver, 10).until(EC.presence_of_element_located((By.NAME, "user_idp")))
+    # Select(select).select_by_visible_text("Ιόνιο Πανεπιστήμιο")
+
+    while True:
+        try:
+            arrow = driver.find_elements_by_class_name('select2-selection__arrow')
+        except:
+            pass
+            #print('except 4')
+        finally:
+            #print('finally 4')
+            if arrow:
                 break
-        while True:
-            try:
-                driver.find_element_by_link_text('Ελληνικά').click()
-            except:
-                pass
-                #print('except 3')
-            finally:
-                #print('finally 3')
+    arrow[0].click()
+    #sys.exit()
+
+    #sleep(.1)
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+
+    while True:
+        try:
+            li = driver.find_elements_by_tag_name('li')
+        except:
+            pass
+            #print('except 5')
+        finally:
+            #print('finally 5')
+            if li:
                 break
+    for n in li:
+        #print(n.text + '\n')
+        if n.text == 'Ιόνιο Πανεπιστήμιο':
+            n.click()
+            break
 
-        #sleep(.1)
-        wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-        # select = wait(driver, 10).until(EC.presence_of_element_located((By.NAME, "user_idp")))
-        # Select(select).select_by_visible_text("Ιόνιο Πανεπιστήμιο")
-
-        arrow = driver.find_elements_by_class_name('select2-selection__arrow')
-        while True:
-            try:
-                arrow = driver.find_elements_by_class_name('select2-selection__arrow')
-            except:
-                pass
-                #print('except 4')
-            finally:
-                #print('finally 4')
-                if arrow:
-                    break
-        arrow[0].click()
-        #sys.exit()
-
-        #sleep(.1)
-        wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-
-        li = driver.find_elements_by_tag_name('li')
-        while True:
-            try:
-                li = driver.find_elements_by_tag_name('li')
-            except:
-                pass
-                #print('except 5')
-            finally:
-                #print('finally 5')
-                if li:
-                    break
-        for n in li:
-            #print(n.text + '\n')
-            if n.text == 'Ιόνιο Πανεπιστήμιο':
-        #sleep(.1)
-               wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
-        #driver.refresh()
-
-        user_field = driver.find_element_by_id('username')
-        user_field.send_keys('username')
-        while True:
-            try:
-                user_field = driver.find_element_by_id('username')
-            except:
-                pass
-                #print('except 6')
-            finally:
-                #print('finally 6')
-                if user_field:
-                    break
-        user_field.send_keys('usrname')
-        pass_field = driver.find_element_by_id('password')
-        pass_field.send_keys('passwd')
-        pass_field.send_keys('passwd!')
-        buttons = driver.find_elements_by_id('submitForm')[0].click()
-
-        sleep(3)
-        #wait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'td')))
-        td = driver.find_elements_by_tag_name('td')[1:-4]
-        while True:
-            try:
-                td = driver.find_elements_by_tag_name('td')
-            except:
-                pass
-                #print('except 7')
-            finally:
-                #print('finally 7')
-                if td:
-                    break
-        out = {}
-        for i,n in enumerate(td):
-            if i % 2 == 0:
-                key = n.text
-            else:
-                out[key] = n.text
-        outl = []
-        ind = 0
-        for n in td:
-            if n.text != '':
-                outl.append(n.text)
-        out = to_dict(outl[:-4])
-        print(out)
-        print('\n')
-        for n in out.keys():
-            print('{0}: {1}'.format(n, out[n]))
-        l = driver.find_elements_by_tag_name('a')
-        for n in l:
-    #print(n.text)
-            if 'Δηλώσεις Συγγραμμάτων' in n.text:
+    #sleep(.1)
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+    buttons = driver.find_elements_by_tag_name('button')
+    if len(buttons) == 1:
+        buttons[0].click()
+    else:
+        for n in buttons:
+            if n.text == 'Επιβεβαίωση':
                 n.click()
                 break
-#driver.quit()
+
+    #sleep(.1)
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+    #driver.refresh()
+    while True:
+        try:
+            user_field = driver.find_element_by_id('username')
+        except:
+            pass
+            #print('except 6')
+        finally:
+            #print('finally 6')
+            if user_field:
+                break
+    user_field.send_keys('')
+    pass_field = driver.find_element_by_id('password')
+    pass_field.send_keys('')
+    buttons = driver.find_elements_by_id('submitForm')[0].click()
+
+
+    wait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'td')))
+    td = driver.find_elements_by_tag_name('td')[:10]
+    out = {}
+    for i,n in enumerate(td):
+        if i % 2 == 0:
+            key = n.text
+        else:
+            out[key] = n.text
+    print(out)
+    print('\n')
+    for n in out.keys():
+        print('{0} {1}'.format(n, out[n]))
+    print('\n')
+
+    l = driver.find_elements_by_tag_name('a')
+    #help(l[0])
+    for n in l:
+        if 'υποβολής' in n.text:
+            n.click()
+            break
+
+    sleep(3)
+    #wait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'td')))
+    while True:
+        try:
+            td = driver.find_elements_by_tag_name('td')
+        except:
+            pass
+            #print('except 7')
+        finally:
+            #print('finally 7')
+            if td:
+                break
+    out = {}
+    outl = []
+    ind = 0
+    for n in td:
+        if n.text != '':
+            outl.append(n.text)
+    out = to_dict(outl[:-4])
+    print(out)
+    print('\n')
+    for n in out.keys():
+        print('{0}: {1}'.format(n, out[n]))
+
+    l = driver.find_elements_by_tag_name('a')
+    for n in l:
+        #print(n.text)
+        if 'Δηλώσεις Συγγραμμάτων' in n.text:
+            n.click()
+            break
+
+    #driver.quit()
 
 def exit():
     sys.exit()
@@ -175,8 +195,9 @@ def updates():
 
 
 if(platform.system() == 'Windows'):
+    os.system("mkdir C:\\Users\\%username%\\AppData\\Roaming\\EudoxusAPI")
     bla = "%username%"
-    os.system('curl '+'-L '+'https://service.eudoxus.gr/images/eudoxus-logo.png '+'-o '+'C:\\User\\{bla}\\AppData\\Local\\EudoxusAPI\\logo.png')
+    os.system(f'curl -L https://service.eudoxus.gr/images/eudoxus-logo.png -o C:\\Users\\{bla}\\AppData\\Roaming\\EudoxusAPI\\logo.png')
 
 if platform.system() == 'Linux':
     try:
@@ -196,7 +217,9 @@ if platform.system() == 'Linux':
 
 
 if platform.system() == 'Windows':
-    img = ImageTk.PhotoImage(Image.open(f"C:\\User\\{bla}\\AppData\\Local\\EudoxusAPI\\logo.png"))
+    os.system('cd C:\\Users\\%username%\\AppData\\Roaming\\EudoxusAPI')
+    bla = "%username%"
+    img = ImageTk.PhotoImage(Image.open("logo.png"))
 
 image = Label(root, image = img)
 
