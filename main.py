0d0d33d86943aa52ace34e8223836b6b433a5eac
Spyros Kokotos <33377581+Greekforce1821@users.noreply.github.com>
2022-03-18 21:41:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from selenium import webdriver
+from selenium import webdriver #Importing All of the required libraries to run smoothly the program while executing it!
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait as wait
 from selenium.webdriver.support import expected_conditions as EC
@@ -13,16 +13,16 @@ import requests
 from PIL import Image, ImageTk
 import sys
 
-## install: sudo apt install python3-pyvirtualdisplay
+## install: sudo apt install python3-pyvirtualdisplay #Here I placed the # to show the display, if I remove the # the pop-up windows will be disabled
 #from pyvirtualdisplay import Display
 #display = Display(visible=0, size=(1680,1050))
 #display.start()
-def to_dict(a):
+def to_dict(a):   #Creating a dictionary to web-scrap the html files
     it = iter(a)
     res_dct = dict(zip(it, it))
     return res_dct
     
-def Main():
+def Main():   #The Main Function operates the driver, the automatic sequence between the user and the program
 
     driver = webdriver.Firefox()
 
@@ -33,7 +33,7 @@ def Main():
     wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
     while True:
         try:
-            driver.find_element_by_link_text('Έλεγχος Εισόδου Φοιτητή').click()
+            driver.find_element_by_link_text('Έλεγχος Εισόδου Φοιτητή').click()  #Checking the button "Έλεγχος Εισόδου Χρήστη" in order to proceed to the next url page
         except:
             pass
             #print('except 1')
@@ -42,7 +42,7 @@ def Main():
             break
 
     #sleep(.1)
-    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete') #Checking the button "εδώ" in order to proceed to the next url page
 
     while True:
         try:
@@ -55,7 +55,7 @@ def Main():
             break
     while True:
         try:
-            driver.find_element_by_link_text('Ελληνικά').click()
+            driver.find_element_by_link_text('Ελληνικά').click() #Changing the language from English to Greek
         except:
             pass
             #print('except 3')
@@ -70,7 +70,7 @@ def Main():
 
     while True:
         try:
-            arrow = driver.find_elements_by_class_name('select2-selection__arrow')
+            arrow = driver.find_elements_by_class_name('select2-selection__arrow') #The driver searches through the class element (which can be found if someone clicks the Inspect Element) for the "Ιόνιο Πανεπιστήμιο" from the pop-up menu
         except:
             pass
             #print('except 4')
@@ -101,7 +101,7 @@ def Main():
             break
 
     #sleep(.1)
-    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete')
+    wait(driver, 10).until(lambda d: d.execute_script('return document.readyState') == 'complete') #Completing the request from the above line
     buttons = driver.find_elements_by_tag_name('button')
     if len(buttons) == 1:
         buttons[0].click()
@@ -116,7 +116,7 @@ def Main():
     #driver.refresh()
     while True:
         try:
-            user_field = driver.find_element_by_id('username')
+            user_field = driver.find_element_by_id('username') #Here is the Ionian University SSO, if someone changes the pass_field.send_keys from " to their username and password, there will be granted access to the auth system
         except:
             pass
             #print('except 6')
@@ -130,7 +130,7 @@ def Main():
     buttons = driver.find_elements_by_id('submitForm')[0].click()
 
 
-    wait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'td')))
+    wait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'td'))) #Here the dictionary prints the students information about the Eudoxus System
     td = driver.find_elements_by_tag_name('td')[:10]
     out = {}
     for i,n in enumerate(td):
@@ -152,7 +152,7 @@ def Main():
             break
 
     sleep(3)
-    #wait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'td')))
+    #wait(driver, 10).until(EC.presence_of_element_located((By.TAG_NAME, 'td'))) #Here the driver searches for the "Δηλώσεις Συγγραμμάτων"
     while True:
         try:
             td = driver.find_elements_by_tag_name('td')
@@ -175,18 +175,18 @@ def Main():
     for n in out.keys():
         print('{0}: {1}'.format(n, out[n]))
 
-    l = driver.find_elements_by_tag_name('a')
+    l = driver.find_elements_by_tag_name('a') #Here the while the button is pressed the program proceeds to the states of the canon books
     for n in l:
         #print(n.text)
         if 'Δηλώσεις Συγγραμμάτων' in n.text:
             n.click()
             break
 
-    #driver.quit()
+    #driver.quit()   #Here the driver quits, stops the procedure
 
 def exit():
     sys.exit()
-
+# Some fuctions we will use in the future later
 def bookstatus():
     return
 
@@ -194,9 +194,10 @@ def updates():
     return
 
 
-if(platform.system() == 'Windows'):
+if(platform.system() == 'Windows'): # Here we download eudoxus image from the main url of eudoxus plus we create a directory where we save some data for later use
+    os.system("mkdir C:\\Users\\%username%\\AppData\\Roaming\\EudoxusAPI")
     bla = "%username%"
-    os.system('curl '+'-L '+'https://service.eudoxus.gr/images/eudoxus-logo.png '+'-o '+'C:\\User\\{bla}\\AppData\\Local\\EudoxusAPI\\logo.png')
+    os.system(f'curl -L https://service.eudoxus.gr/images/eudoxus-logo.png -o C:\\Users\\{bla}\\AppData\\Roaming\\EudoxusAPI\\logo.png')
 
 if platform.system() == 'Linux':
     try:
@@ -206,27 +207,29 @@ if platform.system() == 'Linux':
         if os.system('whoami') == 'root\n0':
             os.system('cd /root/.EudoxusAPI')
         pass
-
+# Some basic parameteres for the root window or GUI window like how big is going to be, title of the window and not allowing to the user change size of the window cause its going to break the style of it.
 root = Tk()
 root.geometry("500x500")
 root.wm_title('EudoxusAPI')
 root.resizable(0, 0)
-if platform.system() == 'Linux':
+if platform.system() == 'Linux': # Adding eudoxus image here and checking if it is windows or linux the platform that is running
     img = ImageTk.PhotoImage(Image.open("logo.png"))
 
 
 if platform.system() == 'Windows':
-    img = ImageTk.PhotoImage(Image.open(f"C:\\User\\{bla}\\AppData\\Local\\EudoxusAPI\\logo.png"))
+    os.system('cd C:\\Users\\%username%\\AppData\\Roaming\\EudoxusAPI')
+    bla = "%username%"
+    img = ImageTk.PhotoImage(Image.open("logo.png"))
 
 image = Label(root, image = img)
 
-
+# Adding/configuring some buttons here
 button = Button(root, text="Login to Eudoxus", command = Main)
 button1 = Button(root, text="Exit", command = exit)
 button2 = Button(root, text="Book status", command = bookstatus)
 button3 = Button(root, text="Give me live-updates", command =updates)
 
-
+# Placing my buttons on the GUI app
 image.place(x = 70, y = 70)
 button.place(x=285, y=450)
 button1.place(x=425, y=450)
