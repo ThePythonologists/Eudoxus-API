125c10f23823dd512dd57a84186526b80aa82d56
Giannpavlos <101205138+Giannpavlos@users.noreply.github.com>
2022-03-29 16:03:08 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
 
-server = smtplib.SMTP(host=smtp.gmail.com, port=465)
+server = smtplib.SMTP_SSL(host='smtp.gmail.com', port=465)
 server.login('thepythonilogists@gmail.com', 'mairaki2003')
 
 
 message = MIMEMultipart()
 
 
-message['From'] = 'theputhonologists@gmail.com'
+message['From'] = 'thepythonologists@gmail.com'
 message['To'] = RECIPIENT_ADDRESS
 message['Subject'] = "Απόθεμα Συγγραμμάτων"
 
